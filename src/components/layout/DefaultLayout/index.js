8dbcbb039661cd081d52c08dd6be32058c13c6ec
Vương Phú <dinhphu@MBP-cua-Vuong.AirPort>
2022-08-30 02:58:21 +0900

    update file

--- src/components/layout/DefaultLayout/index.js
+++ src/components/layout/DefaultLayout/index.js
@@ -1,13 +1,16 @@
+import classNames from 'classnames/bind';
 import Header from '~/components/layout/components/Header';
-import Sidebar from './Sidebar';
+import styles from './DefaultLayout.module.scss';
+import Sidebar from '~/components/layout/components/Sidebar';
 
+const cx = classNames.bind(styles);
 function DefaultLayout({ children }) {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className="container">
+            <div className={cx('container')}>
                 <Sidebar />
-                <div className="content">{children}</div>
+                <div className={cx('content')}>{children}</div>
             </div>
         </div>
     );
