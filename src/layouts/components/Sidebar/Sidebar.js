cd9ba94cb349c1c83f9d7d9a244dfa44e8c1e05a
Vương Phú <dinhphu@MBP-cua-Vuong.AirPort>
2022-08-31 03:16:12 +0900

    Sidebar layout-1

--- src/layouts/components/Sidebar/Sidebar.js
+++ src/layouts/components/Sidebar/Sidebar.js
@@ -1,11 +1,30 @@
 import classNames from 'classnames/bind';
+import config from '~/config/config';
 import styles from './Sidebar.module.scss';
+import Menu, { MenuItem } from './Menu';
+import {
+    HomeIcon,
+    HomeActiveIcon,
+    UserGroupActiveIcon,
+    UserGroupIcon,
+    LiveIcon,
+    LiveActiveIcon,
+} from '~/components/Icons/Icons';
 
 const cx = classNames.bind(styles);
 function Sidebar() {
     return (
         <aside className={cx('wrapper')}>
-            <h2>side</h2>
+            <Menu>
+                <MenuItem title="For you" to={config.routes.home} icon={<HomeIcon />} activeIcon={<HomeActiveIcon />} />
+                <MenuItem
+                    title="Following"
+                    to={config.routes.following}
+                    icon={<UserGroupIcon />}
+                    activeIcon={<UserGroupActiveIcon />}
+                />
+                <MenuItem title="Live" to={config.routes.live} icon={<LiveIcon />} activeIcon={<LiveActiveIcon />} />
+            </Menu>
         </aside>
     );
 }
